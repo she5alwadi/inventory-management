ad945ed3dacd98f8d5daf065ca9772dce4ddf606
she5alwadi <she5.alwadi@gmail.com>
2025-04-21 16:41:04 -0400

    Add inventory_management.js with core logic

---
+++ inventory_management.js
@@ -0,0 +1,27 @@
+// Product class
+class Product {
+    constructor(name, price, quantity) { /* Initialize name, price, quantity */ }
+    getTotalValue() { /* Return price * quantity */ }
+    toString() { /* Return product details string */ }
+    static applyDiscount(products, discount) { /* Reduce price by discount % */ }
+}
+
+// PerishableProduct class (inherits Product)
+class PerishableProduct extends Product {
+    constructor(name, price, quantity, expirationDate) { /* Add expirationDate */ }
+    toString() { /* Include expiration date */ }
+}
+
+// Store class
+class Store {
+    constructor() { /* Initialize empty inventory array */ }
+    addProduct(product) { /* Add product to inventory */ }
+    getInventoryValue() { /* Sum total value of products */ }
+    findProductByName(name) { /* Return product or null */ }
+}
+
+// Test function
+function testInventorySystem() {
+    // Create 5 products (3 Product, 2 PerishableProduct)
+    // Add to store, calculate value, apply 15% discount, search for "Milk"
+}
